cf1c863e73d5fe7c264c6183e3416e8514914dfc
ww-steve-ocampo <esteban.ocampo@magicmemories.com>
2022-03-19 15:26:14 -0300

    feat: initialize project

---
+++ README.md
@@ -0,0 +1,14 @@
+# Metamask with React App
+
+A simple project to plugin Metamask with a React App for simple Etherium transactions
+
+1. **yarn** to install dependencies
+
+2. **yarn start** to run project
+
+### Important!
+
+Otherwise in order to show the Account info you'll need to connect the app to a valid Account on the chain ID you are choosing inside the component/wallet/connector.js file.
+
+
+---
---
+++ src/components/wallet/connectors.js
@@ -0,0 +1,6 @@
+import { InjectedConnector } from '@web3-react/injected-connector'
+// 1 = Ethereum Mainnet
+// 42 = Kovan Testnet — Which I will use to connect in a later article using Web3 and Infura which will come in handing is making simple simple transactions to test out my app
+// 1337 = Local Host chain. For this I used Ganache to create a local chain and connect to my MetaMask wallet.
+// Ref > https://chainlist.org/
+export const injected = new InjectedConnector({ supportedChainIds: [1, 42, 1337] })
